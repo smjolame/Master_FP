4ee0df9ff0ecbc6d664b1c280bfc4b45fb8fcba6
Johannes Lamers <johannes.lamers@tu-dortmund.de>
2022-10-24 17:57:54 +0200

    Daten uebertragen und Auswertung begonnen

--- V21_Optisches_Pumpen/plot.py
+++ V21_Optisches_Pumpen/plot.py
@@ -9,6 +9,8 @@ from uncertainties import unumpy as unp
 from uncertainties.unumpy import (nominal_values as noms,std_devs as stds)
 from scipy.stats import sem
 
+from scipy.constants import mu_0 
+
 def abw(exact,approx):
     return (exact-approx)*100/exact  #Abweichnung
 
@@ -30,4 +32,37 @@ def abw(exact,approx):
 #json.dump(Ergebnisse,open('data/Ergebnisse.json','w'),indent=4)
 
 
+
+
+#Daten imortieren:
+f, B_s1, B_s2, B_h1, B_h2 = np.genfromtxt('data/B_felder.csv', delimiter=',', unpack=True)
+U1, T1 = np.genfromtxt('data/Periodendauern1.csv', delimiter=',', unpack=True)
+U2, T2 = np.genfromtxt('data/Periodendauern2.csv', delimiter=',', unpack=True)
+
+
+
+bruchteile_period1 = np.array([1,3,4,5,6,9,10,10,11,10,13,15,16,18])
+bruchteile_period2 = np.array([1,2,3,4,5,7,8,8,9,10,10,8,10,8])
+
+
+
+T1 = T1/bruchteile_period1 
+T2 = T2/bruchteile_period2 
+
+
+#Test-Plots
+plt.plot(f,B_s1*0.1+B_h1*0.3)
+plt.plot(f,B_s2*0.1+B_h2*0.3)
+plt.show()
+plt.clf()
+plt.plot(U1,T1)
+plt.plot(U2,T2)
+plt.show()
+
+
+
+# Belder berechnen:
+
+def B_helmholtz(N,R,I):
+    return 8/(np.sqrt(125))*mu_0*N*I/R
 
