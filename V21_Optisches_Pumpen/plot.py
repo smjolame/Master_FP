196ada3c4487f495c29aaf108328032743f3e0a9
Johannes Lamers <johannes.lamers@tu-dortmund.de>
2022-10-28 19:58:33 +0200

    alle Werte ausgerechnet

--- V21_Optisches_Pumpen/plot.py
+++ V21_Optisches_Pumpen/plot.py
@@ -9,7 +9,7 @@ from uncertainties import unumpy as unp
 from uncertainties.unumpy import (nominal_values as noms,std_devs as stds)
 from scipy.stats import sem
 
-from scipy.constants import mu_0 , h , elementary_charge , electron_mass, hbar
+from scipy.constants import mu_0 , h , elementary_charge , electron_mass, hbar , eV
 
 mu_B = elementary_charge*hbar/(2*electron_mass)
 
@@ -41,6 +41,7 @@ N_h = 154
 R_v = 0.11735 #m
 N_v = 20
 
+#Konvention: Rb87 = 1, Rb85 = 2 
 
 #Daten imortieren:
 f_kilo, b_s1, b_s2, b_h1, b_h2 = np.genfromtxt('data/B_felder.csv', delimiter=',', unpack=True)
@@ -81,13 +82,17 @@ B_2 = (B_s2+B_h2)#T
 B_v = B_helmholtz(N_v,R_v,2.3*0.1)#T
 
 #Test-Plots
-#plt.plot(f,B_1, 'x')
-#plt.plot(f,B_2, 'x')
-#plt.show()
-#plt.clf()
-#plt.plot(U1,T1,'x')
-#plt.plot(U2,T2,'x')
-#plt.show()
+
+plt.plot(f,B_1, 'x')
+plt.plot(f,B_2, 'x')
+plt.grid()
+plt.show()
+plt.clf()
+plt.plot(U1,T1,'x')
+plt.plot(U2,T2,'x')
+plt.grid()
+plt.show()
+plt.clf()
 
 def Fit_Gerade (x,a,b):
     return a*x+b
@@ -107,7 +112,9 @@ plt.plot(f,B_1, 'x')
 plt.plot(f_lin, Fit_Gerade(f_lin,a1.n,b1.n))
 plt.plot(f,B_2, 'x')
 plt.plot(f_lin, Fit_Gerade(f_lin,a2.n,b2.n))
+plt.grid()
 plt.show()
+plt.clf()
 
 #g-Faktoren
 def g_faktor(a):
@@ -134,3 +141,87 @@ print(I1,I2)
 #Isotopenverhältnis
 Isotope_Ratio = 1/2
 
+
+#Quadratischer Zeeman Effekt:
+def delta_E(B,g_F,M_F,E_Hyperfein):
+    return g_F*mu_B*B+(1-2*M_F)*(g_F*mu_B*B)**2/E_Hyperfein
+
+
+# Hyperfeinaufspaltungen von Rb87 und Rb85
+E_Hyper_1 = 4.53*10**(-24) #J
+E_Hyper_2 = 2.01*10**(-24) #J
+del_E1 = delta_E(B_1[-1],g1,2,E_Hyper_1)
+del_E2 = delta_E(B_2[-1],g2,3,E_Hyper_2)
+
+del_E1_eV = del_E1/eV
+del_E2_eV = del_E2/eV
+
+print(del_E1_eV, del_E2_eV)
+
+
+######################
+#Exp Sättigung
+
+def expo(t,A,b,c):
+    return A+b*np.exp(c*t)
+
+
+
+t1_ ,data1_ = np.genfromtxt('data/Exp_Anst_1.csv', delimiter=',', unpack=True)
+t2_ ,data2_ = np.genfromtxt('data/Exp_Anst_2.csv', delimiter=',', unpack=True)
+data1 = np.cumsum(data1_)
+data2 = np.cumsum(data2_)
+t1 = t1_*25/20 #ms
+t2 = t2_*25/20 #ms
+
+params_exp1, cov_exp1 = curve_fit(expo,t1, data1, p0=[0,0,-1])
+A_e1 = ufloat(params_exp1[0],np.absolute(cov_exp1[0][0])**0.5)
+b_e1 = ufloat(params_exp1[1],np.absolute(cov_exp1[1][1])**0.5)
+c_e1 = ufloat(params_exp1[2],np.absolute(cov_exp1[2][2])**0.5)
+
+params_exp2, cov_exp2 = curve_fit(expo,t2, data2, p0=[0,0,-1])
+A_e2 = ufloat(params_exp2[0],np.absolute(cov_exp2[0][0])**0.5)
+b_e2 = ufloat(params_exp2[1],np.absolute(cov_exp2[1][1])**0.5)
+c_e2 = ufloat(params_exp2[2],np.absolute(cov_exp2[2][2])**0.5)
+
+
+
+t_lin1 = np.linspace(t1[0],t1[-1],1000)
+plt.plot(t_lin1, expo(t_lin1, A_e1.n, b_e1.n, c_e1.n))
+plt.plot(t1,data1,'x')
+t_lin2 = np.linspace(t2[0],t2[-1],1000)
+plt.plot(t_lin2, expo(t_lin2, A_e2.n, b_e2.n, c_e2.n))
+plt.plot(t2,data2,'x')
+plt.grid()
+plt.show()
+plt.clf()
+
+
+#Periodendauern
+
+#Hyperbolische Fitfunktion:
+def hyper(T,a,b,c):
+    return a+b/(T-c)
+
+params_period1, cov_period1 = curve_fit(hyper,U1, T1, p0=[0,3,0])
+a_p1 = ufloat(params_period1[0],np.absolute(cov_period1[0][0])**0.5)
+b_p1 = ufloat(params_period1[1],np.absolute(cov_period1[1][1])**0.5)
+c_p1 = ufloat(params_period1[2],np.absolute(cov_period1[2][2])**0.5)
+
+
+params_period2, cov_period2 = curve_fit(hyper,U2, T2, p0=[0,3,0])
+a_p2 = ufloat(params_period2[0],np.absolute(cov_period2[0][0])**0.5)
+b_p2 = ufloat(params_period2[1],np.absolute(cov_period2[1][1])**0.5)
+c_p2 = ufloat(params_period2[2],np.absolute(cov_period2[2][2])**0.5)
+
+
+U_lin1 = np.linspace(U1[0],U1[-1],1000)
+plt.plot(U1,T1,'x')
+plt.plot(U_lin1,hyper(U_lin1,a_p1.n,b_p1.n,c_p1.n))
+U_lin2 = np.linspace(U2[0],U2[-1],1000)
+plt.plot(U2,T2,'x')
+plt.plot(U_lin2,hyper(U_lin2,a_p2.n,b_p2.n,c_p2.n))
+plt.grid()
+plt.show()
+print(a_p1, b_p1, c_p1)
+print(a_p2, b_p2, c_p2)
