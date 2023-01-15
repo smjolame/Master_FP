f49dea1f15a535df42638f4ef18605fa65abe54a
Johannes Lamers <smjolame@tu-dortmund.de>
2023-01-15 16:05:21 +0100

    Auswertung

--- V64_Interferometrie/plot.py
+++ V64_Interferometrie/plot.py
@@ -14,7 +14,7 @@ def abw(exact,approx):
 
 #Kontrast:
 
-phi, U_min, U_max = np.genfromtxt("data/kontrastmessung_Dummy.txt", delimiter=',', unpack=True)
+phi, U_min, U_max = np.genfromtxt("data/kontrast.txt", delimiter=',', unpack=True)
 
 phi_rad = np.radians(phi)#rad
 
@@ -26,6 +26,7 @@ def K_phi(phi,A):
 
 K = K(U_min, U_max)
 
+np.savetxt('build/K.txt',K.T, fmt='%1.2f' , delimiter = '  &  ')
 
 
 params, cov = curve_fit(K_phi,phi_rad,K)
@@ -43,7 +44,7 @@ plt.legend(loc=1)
 tick_pos= [0, np.pi/2 , np.pi]
 labels = ['0', r'$\frac{\pi}{2}$', r'$\pi$']
 plt.xticks(tick_pos,labels)
-plt.xlabel(r'$\Theta \mathbin{/} \si{\radian}$')
+plt.xlabel(r'$\phi \mathbin{/} \si{\radian}$')
 plt.ylabel(r'$K$')
 plt.ylim([0,1.4])
 plt.grid()
@@ -53,7 +54,7 @@ plt.clf()
 
 
 #Brechungsindex Glas
-durchgang, M = np.genfromtxt("data/Brechungsindex_Glas_Dummy.txt", delimiter=',', unpack=True) 
+durchgang, M = np.genfromtxt("data/Brechungsindex_Glas.txt", delimiter=',', unpack=True) 
 lambda_0 = 632.99 * 10**(-9) #m 
 theta_0 = np.radians(10) #rad
 D = 1 * 10**(-3) #m
@@ -64,12 +65,21 @@ def n_func(M_,theta):
     return 1/(1-(M_*lambda_0)/(2*D*theta*theta_0))
 
 n_glas = n_func(M,theta_rad)
-np.savetxt('build/n_Glas.txt',n_glas.T, delimiter = '  &  ')
+np.savetxt('build/n_Glas.txt',n_glas.T, fmt='%1.8f' , delimiter = '  &  ')
+
+
+n_glas_mean = np.mean(n_glas)
+n_glas_err = np.std(n_glas)
+n_glas_exp=np.array([n_glas_mean,n_glas_err])
+
+np.savetxt('build/n_Glas_mean.txt',n_glas_exp, fmt='%1.8f' , delimiter = '  &  ')
+
+
 #########Brechungsindex Gas:
 ####Luft
 L=ufloat(0.1,0.0001) #m
-T = 21.1+273.15 #K
-p, M1,M2,M3,M4 = np.genfromtxt("data/Brechungsindex_Luft_Dummy.txt", delimiter=',', unpack=True)
+T = 20.7+273.15 #K
+p, M1,M2,M3 = np.genfromtxt("data/Brechungsindex_Luft.txt", delimiter=',', unpack=True)
 
 
 def n_func2(M_):
@@ -79,25 +89,34 @@ def n_func2(M_):
 n_luft1 = n_func2(M1)
 n_luft2 = n_func2(M2)
 n_luft3 = n_func2(M3)
-n_luft4 = n_func2(M4)
 
+n_luft1_mean = np.mean(n_luft1)
+n_luft2_mean = np.mean(n_luft2)
+n_luft3_mean = np.mean(n_luft3)
+
+n_luft1_std = np.std(n_luft1)
+n_luft2_std = np.std(n_luft2)
+n_luft3_std = np.std(n_luft3)
+
+np.savetxt('build/n_Luft.txt',n_luft1,fmt='%1.8f', delimiter = '  &  ')
 
 #Nominal werte
 n_luft1_n = unp.nominal_values(n_luft1)
 n_luft2_n = unp.nominal_values(n_luft2)
 n_luft3_n = unp.nominal_values(n_luft3)
-n_luft4_n = unp.nominal_values(n_luft4)
 
-n_luft_Tabelle_n = np.array([n_luft1_n,n_luft2_n,n_luft3_n,n_luft4_n]).T
+
+n_luft_Tabelle_n = np.array([n_luft1_n,n_luft2_n,n_luft3_n]).T
 
 # n Daten abspeichern
-np.savetxt('build/n_Luft.txt',n_luft_Tabelle_n, delimiter = '  &  ')
+np.savetxt('build/n_Luft.txt',n_luft_Tabelle_n,fmt='%1.8f', delimiter = '  &  ')
 
 
 ####Gas
 R = const.R
 def n_func_gas(p_,a,b):
-    return a/(T*R)*p_+b
+    return np.sqrt(1 + a*p_/(R*T)) + b
+
 
 ##### curvefits für Gas
 params1, cov1 = curve_fit(n_func_gas, p, n_luft1_n)
@@ -112,27 +131,23 @@ params3, cov3 = curve_fit(n_func_gas, p, n_luft3_n)
 a_gas3 = ufloat(params3[0],np.absolute(cov3[0][0])**0.5)
 b_gas3 = ufloat(params3[1],np.absolute(cov3[1][1])**0.5)
 print('a_gas_3:',a_gas3,'b_gas_3:',b_gas3)
-params4, cov4 = curve_fit(n_func_gas, p, n_luft4_n)
-a_gas4 = ufloat(params4[0],np.absolute(cov4[0][0])**0.5)
-b_gas4 = ufloat(params4[1],np.absolute(cov4[1][1])**0.5)
-print('a_gas_4:',a_gas4,'b_gas_4:',b_gas4)
 
 
 
 ######## Plots für Gas
 p_lin=np.linspace(p[0], p[-1],1000)
 
+
+
 plt.plot(p,unp.nominal_values(n_luft1), 'x', label = 'Messwerte 1', c = 'b')
 plt.plot(p_lin,n_func_gas(p_lin,a_gas1.n,b_gas1.n), label = 'Fit 1', c = 'b')
 
-plt.plot(p,unp.nominal_values(n_luft2), 'x', label = 'Messwerte 2',c = 'm')
-plt.plot(p_lin,n_func_gas(p_lin,a_gas2.n,b_gas2.n), label = 'Fit 2', c = 'm')
+plt.plot(p,unp.nominal_values(n_luft2), 'x', label = 'Messwerte 2',c = 'r')
+plt.plot(p_lin,n_func_gas(p_lin,a_gas2.n,b_gas2.n), label = 'Fit 2', c = 'r')
 
 plt.plot(p,unp.nominal_values(n_luft3), 'x', label = 'Messwerte 3',c = 'k')
 plt.plot(p_lin,n_func_gas(p_lin,a_gas3.n,b_gas3.n), label = 'Fit 3',c = 'k')
 
-plt.plot(p,unp.nominal_values(n_luft4), 'x', label = 'Messwerte 4',c = 'r')
-plt.plot(p_lin,n_func_gas(p_lin,a_gas4.n,b_gas4.n), label = 'Fit 4',c = 'r')
 plt.xlabel(r'$p \mathbin{/} \si{\milli\bar}$')
 plt.ylabel(r'$n$')
 plt.legend(fontsize='small')
